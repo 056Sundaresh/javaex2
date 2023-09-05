79a570855aad0238fb6a8626aad7126f301d9853
056Sundaresh <121413949+056Sundaresh@users.noreply.github.com>
2023-09-05 10:20:32 +0530

    Update JavaApplication1.java

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -4,18 +4,53 @@
  * and open the template in the editor.
  */
 package javaapplication1;
-
-/**
- *
- * @author test01
- */
-public class JavaApplication1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import java.util.Scanner;
+public class JavaApplication20 {
+public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.print("Enter Length : ");
+        float a=obj.nextFloat();
+        System.out.print("Enter Width : ");
+        float b=obj.nextFloat();
+        Rectangle rect = new Rectangle(a,b);            
+        System.out.println("Length : " + rect.getLength());
+        System.out.println("Width : " + rect.getWidth());
+        System.out.println("Area : " + rect.calculateArea());
+        System.out.println("Perimeter : " + rect.calculatePerimeter());
     }
-    
+public static class Rectangle {
+    private float length;
+    private float width;
+   
+    public Rectangle(float length, float width) {
+        this.length = length;
+        this.width = width;
+    }
+   
+    public float getLength() {
+        return length;
+    }
+   
+    public void setLength(float length) {
+        this.length = length;
+    }
+   
+    public float getWidth() {
+        return width;
+    }
+   
+    public void setWidth(float width) {
+        this.width = width;
+    }
+   
+    public float calculateArea() {
+        return length * width;
+    }
+   
+    public float calculatePerimeter() {
+        return 2 * (length + width);
+    }
+   
+       
+    }
 }
